b3182d3673ff5c99d484d17b6e990ceab90a2632
zouooh <zouooh@gmail.com>
2022-02-24 10:51:28 +0800

    feat. nullo

---
+++ src/com/zouol/design/behavoir/nullo/AbstractCustomer.java
@@ -0,0 +1,7 @@
+package com.zouol.design.behavoir.nullo;
+
+public abstract class AbstractCustomer {
+    protected String name;
+    public abstract boolean isNil();
+    public abstract String getName();
+}
